185ae67d6eabff10dd17fd0d8e83ad987db64041
Ethan Ferguson <sometimescode@gmail.com>
2020-12-26 17:42:30 -0500

    avoided the std::thread problem in a bad way

--- FalseSharing/false_sharing.cpp
+++ FalseSharing/false_sharing.cpp
@@ -1,9 +1,18 @@
 
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include <cstdio>
 #include <ctime>
+#include <unistd.h>
+
+std::mutex mtx;
+std::condition_variable cv;
 
 void add_fn(int* a, int n){
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock);
+    lock.unlock();
     for(;n>0;n--){
         *a += n + (*a * n)/4;
     }
@@ -11,13 +20,26 @@ void add_fn(int* a, int n){
 
 int main(){
     int N = 10000000;
+    int in = 0;
     {
-        int a1[2];
+        std::thread tr(add_fn, &in, N);
+        usleep(1000);
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.notify_all();
+        lock.unlock();
+        tr.join();
+    }
+    {
+        int a[2] = {0};
         timespec ptime, time;
         float dtime;
+        std::thread tr1(add_fn, a, N);
+        std::thread tr2(add_fn, a+1, N);
+        usleep(1000);
+        std::unique_lock<std::mutex> lock(mtx);
         clock_gettime(CLOCK_REALTIME, &ptime);
-        std::thread tr1(add_fn, a1, N);
-        std::thread tr2(add_fn, a1+1, N);
+        cv.notify_all();
+        lock.unlock();
         tr1.join();
         tr2.join();
         clock_gettime(CLOCK_REALTIME, &time); 
@@ -25,12 +47,16 @@ int main(){
         printf("false share: %f sec\n", dtime);
     }
     {
-        int a[128];
+        int a[128] = {0};
         timespec ptime, time;
         float dtime;
-        clock_gettime(CLOCK_REALTIME, &ptime);
         std::thread tr1(add_fn, a, N);
         std::thread tr2(add_fn, a+64, N);
+        usleep(1000);
+        std::unique_lock<std::mutex> lock(mtx);
+        clock_gettime(CLOCK_REALTIME, &ptime);
+        cv.notify_all();
+        lock.unlock();
         tr1.join();
         tr2.join();
         clock_gettime(CLOCK_REALTIME, &time); 
